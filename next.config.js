672f41178a8cef1443f9283d3d867176689f8d33
naotaroimo <noymaeyama@gmail.com>
2020-12-03 23:16:58 +0900

    end:nextjs with typescript:34

--- next.config.js
+++ next.config.js
@@ -2,5 +2,13 @@ module.exports = {
     env: {
         MY_ENV: process.env.MY_ENV,
         GREETING: process.env.GREETING
+    },
+    serverRuntimeConfig: {
+        //サーバサイド側でのみ利用可能
+        MY_SECRET: process.env.MY_SECRET
+    },
+    publicRuntimeConfig: {
+        //クライアント、サーバサイド側両方で利用可能
+        MY_MESSAGE: "welcome!"
     }
 }
---
+++ pages/envdemo2.tsx
@@ -0,0 +1,34 @@
+import { GetServerSideProps } from "next"
+import getConfig from 'next/config';
+
+const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
+
+// サーバ、クライアント両方
+console.log(publicRuntimeConfig.MY_MESSAGE);
+//サーバサイド側のみ
+console.log(serverRuntimeConfig.MY_SECRET);
+
+const EnvDemo2 =(props:any) =>{
+   return (
+       <div>
+           <div>
+           MY_MESSAGE:{publicRuntimeConfig.MY_MESSAGE}
+           </div>
+           <div>
+           MY_SECRET:{serverRuntimeConfig.MY_SECRET}
+           </div>
+           {JSON.stringify(props,null,4)}
+       </div>
+   );
+}
+
+export default EnvDemo2
+
+export const getServerSideProps:GetServerSideProps = (ctx) =>{
+   return {
+       props:{
+           MY_MESSAGE:publicRuntimeConfig.MY_MESSAGE,
+           MY_SECRET:serverRuntimeConfig.MY_SECRET
+       }
+   };
+}
